077befaa3cc01db5dd81b39064d21c0cdc852170
ccc <mchadchay@hotmail.com>
2018-04-25 16:20:28 +0700

    update

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -13,13 +13,7 @@ module.exports = {
    * `HomeController.index()`
    */
   index: function (req, res) {
-
-      var express = require('express');
-      var app = express();
-      var path = require('path');
-      app.get('/', function (req, res) {
-          res.sendfile(path.join(__dirname + '/index.html'));
-      });
-  }
+    res.view("index");
+},
 };
 
