a40c7aa72eeb49eab4bc010853e3c8126f607d96
ccc <mchadchay@hotmail.com>
2018-04-25 22:32:46 +0700

    test

--- config/routes.js
+++ config/routes.js
@@ -67,7 +67,7 @@ module.exports.routes = {
   // },
 
   '/dashboard': { view: 'dashboard' },
-  '/home': { view: 'homepage' },
+  '/home': { view: 'index' },
 
   'get /district/province/:pid': {
     controller: 'District',
