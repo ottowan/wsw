6012ee19f57769993a9bddaf3cd78f6471e6639f
pentaho <ottowan@gmail.com>
2018-05-15 12:28:56 +0700

    [Edited] แก้การแสดงผลข้อมูลแมพ ในหน้าค้นหา

--- api/controllers/SearchController.js
+++ api/controllers/SearchController.js
@@ -8,15 +8,36 @@
 module.exports = {
 
     index: function (req, res) {
+
+        var searchBy = req.param('by')
+        if (searchBy == "route"){
+            this.byRoute(req, res)
+        }else if(searchBy == "partner"){
+
+        }
+
+    },
+
+    byRoute: function (req, res) {
         let responses = {
             by: req.param('by'),
+            cartype: req.param('cartype'),
             startpoint: req.param('startpoint'),
             endpoint: req.param('endpoint')
         }
-
-
         console.log(responses)
         return res.view("search_route", responses)
     },
+
+    byPartner: function (req, res) {
+        let responses = {
+            by: req.param('by'),
+            cartype: req.param('cartype'),
+            startpoint: req.param('startpoint'),
+            endpoint: req.param('endpoint')
+        }
+        console.log(responses)
+        return res.json(responses)
+    },    
 };
 
